2ba902a16b5ea3c287e313b96275b3f74f2d8088
Sean Marlow <sean.marlow@suse.com>
2021-07-01 10:01:39 -0500

    Add functions to import and delete key pairs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -21,9 +21,17 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from aliyun_img_utils.aliyun_exceptions import AliyunException
+from pytest import raises
 from unittest.mock import patch, Mock
 
-from aliyun_img_utils.aliyun_utils import put_blob, click_progress_callback
+from aliyun_img_utils.aliyun_utils import (
+    put_blob,
+    click_progress_callback,
+    get_compute_client,
+    import_key_pair,
+    delete_key_pair
+)
 
 
 @patch('aliyun_img_utils.aliyun_utils.click')
@@ -47,3 +55,42 @@ def test_put_blob(mock_oss2):
         'tests/data/blob.vhd',
         progress_callback=callback
     )
+
+
+@patch('aliyun_img_utils.aliyun_utils.AcsClient')
+def test_get_compute_client(mock_acs):
+    client = Mock()
+    mock_acs.return_value = client
+
+    result = get_compute_client('123', '321', 'cn-beijing')
+    assert result == client
+
+    # Exception
+    mock_acs.side_effect = Exception('Invalid credentials!')
+
+    with raises(AliyunException):
+        get_compute_client('123', '321', 'cn-beijing')
+
+
+def test_import_key():
+    client = Mock()
+    import_key_pair('key123', '321', client)
+    assert client.do_action_with_exception.call_count == 1
+
+    # Exception
+    client.do_action_with_exception.side_effect = Exception('Duplicate key!')
+
+    with raises(AliyunException):
+        import_key_pair('key123', '321', client)
+
+
+def test_delete_key():
+    client = Mock()
+    delete_key_pair('key123', client)
+    assert client.do_action_with_exception.call_count == 1
+
+    # Exception
+    client.do_action_with_exception.side_effect = Exception('Invalid key!')
+
+    with raises(AliyunException):
+        delete_key_pair('key123', client)
